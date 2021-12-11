c5e3c0b8b8db182afa1afe6bf54e48909d345372
suhyun choi <95855890+suhyun96@users.noreply.github.com>
2021-12-11 21:29:17 +0900

    Update dartgame.java
    숫자 입력을 뒤로 빼서 저장 
    *표의 개념 제대로 이해완료

--- dartgame.java
+++ dartgame.java
@@ -1,64 +1,53 @@
-import java.util.Arrays;
-
-public class dart {
+public class dart2 {
     public static void main(String[] args) {
 
-        String dartResult = "1D2S3T*";
+        String dartResult = "1S*2T*3S";
         int answer = 0;
         int[] answer2 = new int[3];
         // 입력받은 수를 배열로 나눈다.
         // 각 자리마다 숫자인지 알파벳인지 특문인지에 따른 if
-        // 1-10까지라면 그거 int로 변경해야함
 
         String[] array_dart = dartResult.split("");
-        // int flag = 0;
-        int[] num = new int[3];
-        int count2 = 0;
+        // int[] num = new int[3];
+        int count = 0;
 
         for (int i = 0; i < array_dart.length; i++) {
             char ch = dartResult.charAt(i);
-            if (48 <= ch && ch <= 57) {
-                // num[count2] = Character.getNumericValue(ch);
+            if (ch == 'S') {
 
-                if (Character.getNumericValue(ch) == 0) {
-                    if (num[count2 - 1] == 1) {
-                        num[count2 - 1] = 10;
-                    } else {
-                        num[count2] = Character.getNumericValue(ch);
-                        count2++;
-                    }
-                }
-                // else(num[count2] = Character.getNumericValue(ch))
-                else {
-                    num[count2] = Character.getNumericValue(ch);
-                    count2++;
-                }
-
-            } else if (ch == 'S') {
-                answer2[count2 - 1] = num[count2 - 1];
             } else if (ch == 'D') {
-                answer2[count2 - 1] = num[count2 - 1] * num[count2 - 1];
+                answer2[count - 1] = answer2[count - 1] * answer2[count - 1];
             } else if (ch == 'T') {
-                answer2[count2 - 1] = num[count2 - 1] * num[count2 - 1] * num[count2 - 1];
+                answer2[count - 1] = answer2[count - 1] * answer2[count - 1] * answer2[count - 1];
             } else if (ch == '*') {
+                if (count == 1) {
+                    answer2[count - 1] = answer2[count - 1] * 2;
 
-                if (count2 == 0) {
-                    answer2[count2 - 1] = 2 * answer2[count2 - 1];
-                } else if (count2 == 1) {
-                    answer2[0] = 2 * answer2[0];
-                    answer2[1] = 2 * answer2[1];
-                } else if (count2 == 2 || count2 == 3) {
-                    answer2[0] = 2 * answer2[0];
-                    answer2[1] = 2 * answer2[1];
-                    answer2[2] = 2 * answer2[2];
+                } else if (count > 1) {
+                    answer2[count - 2] = answer2[count - 2] * 2;
+                    answer2[count - 1] = answer2[count - 1] * 2;
+
                 }
 
             } else if (ch == '#') {
-                answer2[count2 - 1] = -answer2[count2 - 1];
+                answer2[count - 1] = -answer2[count - 1];
+
+            } else {
+                // 숫자 전용
+                answer2[count] = Character.getNumericValue(ch);
+                if (answer2[count] == 1) {
+                    if (Character.getNumericValue(dartResult.charAt(i + 1)) == 0) {
+                        answer2[count] = 10;
+                        i++;
+                    }
+                }
+                count++;
             }
 
         }
-        answer = Arrays.stream(answer2).sum();
+        for (int i = 0; i < 3; i++) {
+            answer = answer + answer2[i];
+        }
         System.out.print(answer);
 
     }
